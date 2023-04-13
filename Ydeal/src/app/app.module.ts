962cbee7ccc3562bb703a5f44346cbd1ba353243
clementSIO <116883467+Clementetiennedev@users.noreply.github.com>
2023-04-13 12:28:15 +0200

    Architecture

--- Ydeal/src/app/app.module.ts
+++ Ydeal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccueilComponent} from "../components/accueil/accueil.component";
+import {LoginPageComponent} from "../components/login-page/login-page.component";
+import {NavbarComponent} from "../components/navbar/navbar.component";
+import {RegisterPageComponent} from "../components/register-page/register-page.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    LoginPageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
   ],
   imports: [
     BrowserModule,
