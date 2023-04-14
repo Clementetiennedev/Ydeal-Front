d09400f8c226c642bcf3f41185c90710ee01b020
clementSIO <116883467+Clementetiennedev@users.noreply.github.com>
2023-04-14 09:58:11 +0200

    add site vitrine template + navbar button

--- Ydeal/src/app/app-routing.module.ts
+++ Ydeal/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {LoginPageComponent} from "../components/login-page/login-page.component"
 import {RegisterPageComponent} from "../components/register-page/register-page.component";
 import {ProfilComponent} from "../components/profil/profil.component";
  import { AuthGuard } from "@angular/fire/auth-guard";
+import {SiteVitrineComponent} from "../components/site-vitrine/site-vitrine.component";
 
 const routes: Routes = [
   { path : '' , component : AccueilComponent},
   { path : 'login' , component : LoginPageComponent},
   { path : 'register' , component : RegisterPageComponent},
-  { path : 'profil' , component : ProfilComponent}
+  { path : 'profil' , component : ProfilComponent},
+  { path : 'site-vitrine' , component : SiteVitrineComponent}
 ];
 
 @NgModule({
---
+++ Ydeal/src/components/site-vitrine/site-vitrine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-site-vitrine',
+  templateUrl: './site-vitrine.component.html',
+  styleUrls: ['./site-vitrine.component.scss']
+})
+export class SiteVitrineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
